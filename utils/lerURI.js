854f9195c27eb8424c5a0335d1444be0300ff5a8
thiagouxd <thiago.uxbr@gmail.com>
2021-10-24 16:24:59 -0300

    Adicionando Jimp para imagens

--- utils/lerURI.js
+++ utils/lerURI.js
@@ -1,13 +1,27 @@
+var Jimp = require('jimp/es');
+
 const lerURI = (e) => {
   if (e.target.files) {
     const files = Array.from(e.target.files);
 
     return Promise.all(
       files.map((file, index) => {
+        const imagemProcessada = Jimp.read(file)
+          .then(
+            image => {
+              return image
+                .resize(1000, 1000)
+                .quality(60)
+            }
+          )
+
+        console.log(imagemProcessada)
+
         return new Promise((resolve, reject) => {
           const reader = new FileReader();
+          console.log()
           reader.addEventListener("load", (ev) => {
-            resolve({result: ev.target.result, files: e.target.files[index]});
+            resolve({ result: ev.target.result, files: e.target.files[index] });
           });
           reader.addEventListener("error", reject);
           reader.readAsDataURL(file);
