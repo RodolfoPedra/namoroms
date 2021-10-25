1fd3da12e33d2f29934db2faf053bc0dee50a18b
Thiago Menezes <31048708+thiagouxd@users.noreply.github.com>
2021-10-25 12:23:26 -0300

    Ajuste nas controllers

--- interface/controllers/criacao-conta.js
+++ interface/controllers/criacao-conta.js
@@ -3,8 +3,8 @@ import autenticar from 'interface/services/autenticar';
 
 export const postUsuario = async (dataUser) => {
     const { data } = await autenticar({
-        "username": process.env.LOGIN,
-        "password": process.env.LOGIN_KEY
+        "username": process.env.NEXT_PUBLIC_LOGIN,
+        "password": process.env.NEXT_PUBLIC_LOGIN_KEY
     });
 
     const result = await postData(dataUser, data.token);
